5f764abab2c356cd944b2101e373837f2996021f
Anshul Goyal <anshulpeehu3@gmail.com>
2021-04-07 02:12:45 +0530

    added 5 pages

--- app.js
+++ app.js
@@ -7,19 +7,23 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended :true}))
 
 var l=[];
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 407; i++) {
     l.push(i+1);
     
 }
-
-app.get("/",function (req,res) {
-    res.render("index.ejs",{question:l});
+// app.get("/",function (req,res) {
+//     res.render("index.ejs",{question:l.slice(0,100)});
+// })
+for (let i = 1; i < 6; i++) {
+   
+    app.get("/"+String(i),function (req,res) {
+        res.render("index.ejs",{question:l.slice((i-1)*100,(i)*100)});
 })
-
+}
 app.post("/",function (req,res) {
     var newQues=req.body.newQues;
     l.push(newQues);
-    res.redirect("/");
+    res.redirect("/5");
 })
 
 app.listen(3000);
