38251b8f98b0e5319d35233259ded894d40ab9b4
Anshul Goyal <anshulpeehu3@gmail.com>
2021-04-11 02:46:34 +0530

    added database

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const bodyParser=require("body-parser");
+const mongoose = require("mongoose");
 
 const app=express();
 app.set('view engine', 'ejs');
 app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended :true}))
 
-var l=[];
-for (let i = 0; i < 407; i++) {
-    l.push(i+1);
-    
-}
-// app.get("/",function (req,res) {
-//     res.render("index.ejs",{question:l.slice(0,100)});
-// })
+mongoose.connect('mongodb://localhost:27017/questionsDB', {useNewUrlParser: true, useUnifiedTopology: true});
+/****************************************************************************************************** */
+
+/*************add user in schema after login */
+const schemaQuestion= new mongoose.Schema({
+    questionText: String,
+    comments:[{commentText:String}]
+
+})
+
+const questions = mongoose.model('Question',schemaQuestion);
+
+
+app.get("/",function (req,res) {
+    res.render("home.ejs");
+})
 for (let i = 1; i < 6; i++) {
    
     app.get("/"+String(i),function (req,res) {
-        res.render("index.ejs",{question:l.slice((i-1)*100,(i)*100)});
+        questions.find({},function (err,questionsUploaded) {
+            res.render("index.ejs",{question:questionsUploaded});
+        })
+        
 })
 }
 app.post("/",function (req,res) {
     var newQues=req.body.newQues;
-    l.push(newQues);
-    res.redirect("/5");
+    var q1 = new questions({questionText:newQues})
+    questions.insertMany([q1]);
+    res.redirect("/1");
 })
 
 app.listen(3000);
