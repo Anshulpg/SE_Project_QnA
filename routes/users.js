117126106ce0ad02e6da1230ae9a2392addc0d8c
Anshul Goyal <anshulpeehu3@gmail.com>
2021-04-30 00:11:15 +0530

    restricted user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+//1//04mLfMU4FJBg_CgYIARAAGAQSNwF-L9Ir2DWRTyBnDQwvoVfpNJXteXk4eGFfIyxWpgojPSq11ktuGa5s8Y0IRipEghfILtAbZJ8
+
 const express = require('express');
 const router =express.Router();
 const bcrypt = require('bcryptjs');
@@ -75,13 +77,13 @@ router.post('/register', function(req,res){
                 }else{
                     
                     const oauth2Client = new OAuth2(
-                        "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com", // ClientID
-                        "OKXIYR14wBB_zumf30EC__iJ", // Client Secret
+                        "1041816779848-ehb6ngglc96q5p8hvujd67vpv9d1qh3u.apps.googleusercontent.com", // ClientID
+                        "_MI73ojAvQr2EQavt5bsR7Zk", // Client Secret
                         "https://developers.google.com/oauthplayground" // Redirect URL
                     );
     
                     oauth2Client.setCredentials({
-                        refresh_token: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w"
+                        refresh_token: "1//04mLfMU4FJBg_CgYIARAAGAQSNwF-L9Ir2DWRTyBnDQwvoVfpNJXteXk4eGFfIyxWpgojPSq11ktuGa5s8Y0IRipEghfILtAbZJ8"
                     });
                     const accessToken = oauth2Client.getAccessToken()
     
@@ -98,17 +100,17 @@ router.post('/register', function(req,res){
                         service: 'gmail',
                         auth: {
                             type: "OAuth2",
-                            user: "goyal.14@iitj.ac.in",
-                            clientId: "386662144086-pp8ac9jge3ieuvimirb9gpa2geum3jdk.apps.googleusercontent.com",
-                            clientSecret: "4gxjOQtYQUPGqD6cBqjuS21R",
-                            refreshToken: "1//04xL2BS5YkGegCgYIARAAGAQSNwF-L9IrEyOnv5l8AncZxHUEgqreBgN27NXQkrPiCJNHfDIFy9-XYVu7gh_1C2EopUKxBJ-1A0Y",
+                            user: "iitjforumhelp@gmail.com",
+                            clientId: "1041816779848-ehb6ngglc96q5p8hvujd67vpv9d1qh3u.apps.googleusercontent.com",
+                            clientSecret: "_MI73ojAvQr2EQavt5bsR7Zk",
+                            refreshToken: "1//04mLfMU4FJBg_CgYIARAAGAQSNwF-L9Ir2DWRTyBnDQwvoVfpNJXteXk4eGFfIyxWpgojPSq11ktuGa5s8Y0IRipEghfILtAbZJ8",
                             accessToken: accessToken
                         },
                     });
     
                     // send mail with defined transport object
                     const mailOptions = {
-                        from: '"Auth Admin" <goyal.14@iitj.ac.in>', // sender address
+                        from: '"Auth Admin" <iitjforumhelp@gmail.com>', // sender address
                         to: email, // list of receivers
                         subject: "Account Verification: NodeJS Auth ✔", // Subject line
                         generateTextFromHTML: true,
@@ -335,13 +337,13 @@ router.post('/forgot',function (req,res) {
             } else {
 
                 const oauth2Client = new OAuth2(
-                    "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com", // ClientID
-                    "OKXIYR14wBB_zumf30EC__iJ", // Client Secret
+                    "1041816779848-ehb6ngglc96q5p8hvujd67vpv9d1qh3u.apps.googleusercontent.com", // ClientID
+                    "_MI73ojAvQr2EQavt5bsR7Zk", // Client Secret
                     "https://developers.google.com/oauthplayground" // Redirect URL
                 );
 
                 oauth2Client.setCredentials({
-                    refresh_token: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w"
+                    refresh_token: "1//04mLfMU4FJBg_CgYIARAAGAQSNwF-L9Ir2DWRTyBnDQwvoVfpNJXteXk4eGFfIyxWpgojPSq11ktuGa5s8Y0IRipEghfILtAbZJ8"
                 });
                 const accessToken = oauth2Client.getAccessToken()
 
@@ -366,17 +368,17 @@ router.post('/forgot',function (req,res) {
                             service: 'gmail',
                             auth: {
                                 type: "OAuth2",
-                                user: "nodejsa@gmail.com",
-                                clientId: "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com",
-                                clientSecret: "OKXIYR14wBB_zumf30EC__iJ",
-                                refreshToken: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
+                                user: "iitjforumhelp@gmail.com",
+                                clientId: "1041816779848-ehb6ngglc96q5p8hvujd67vpv9d1qh3u.apps.googleusercontent.com",
+                                clientSecret: "_MI73ojAvQr2EQavt5bsR7Zk",
+                                refreshToken: "1//04mLfMU4FJBg_CgYIARAAGAQSNwF-L9Ir2DWRTyBnDQwvoVfpNJXteXk4eGFfIyxWpgojPSq11ktuGa5s8Y0IRipEghfILtAbZJ8",
                                 accessToken: accessToken
                             },
                         });
 
                         // send mail with defined transport object
                         const mailOptions = {
-                            from: '"Auth Admin" <nodejsa@gmail.com>', // sender address
+                            from: '"Auth Admin" <iitjforumhelp@gmail.com>', // sender address
                             to: email, // list of receivers
                             subject: "Account Password Reset: NodeJS Auth ✔", // Subject line
                             html: output, // html body
